e7bbf3508fdc14499a7d17dfe7c84cfe7ce42c5d
jrhawkinMTU <hawkinsjr27@gmail.com>
2025-01-25 12:54:32 -0500

    Third person camera improvements

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
+    private const float VERTICAL_MOD = 0.45f;
     /// <summary>
     /// The object that this camera will reference for angle and position.
     /// </summary>
@@ -12,6 +13,15 @@ public class ThirdPersonCamera : MonoBehaviour
     {
         transform.position = follow.position;
         transform.forward = follow.forward;
-        transform.Translate(Vector3.forward * -dist);
+        // Get final position
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, -transform.forward, out hit, dist))
+        {
+            // "collide" with ground
+            transform.position = hit.point;
+            transform.Translate(Vector3.up * VERTICAL_MOD);
+            return;
+        }
+        transform.Translate(Vector3.forward * -dist + Vector3.up * VERTICAL_MOD);
     }
 }
